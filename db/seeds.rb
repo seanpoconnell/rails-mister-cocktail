9ad94604b2347e30765fac68ab855ba6b129a735
sean oconnell <seanoconnell96@gmail.com>
2020-08-21 12:13:57 +0100

    Cocktail routing, controller, views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# add ingredients
 require 'open-uri'
 require 'json'
 
+puts "cleaning ingredients db!"
+Ingredient.delete_all
+Ingredient.reset_pk_sequence
+puts "db cleaned!"
+puts "parsing ingredients"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 
 json = open(url).read
@@ -18,3 +24,21 @@ ingredients.each do |ing|
   ingredient = ing["strIngredient1"]
   Ingredient.create(name: ingredient)
 end
+puts "ingredients added!"
+
+# add cocktails
+puts 'destroying all cocktails!'
+Cocktail.delete_all
+Cocktail.reset_pk_sequence
+# Cocktail.destroy_all
+puts 'DB is clean'
+
+puts 'creating cocktails'
+20.times do |cocktail|
+  ct = Cocktail.create!(
+    name: Faker::Coffee.blend_name
+  )
+  puts "created cocktail number #{cocktail}"
+end
+
+puts "Done!"
